12c1be0b39a7fcbc63252770298666a40006cddc
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-10-05 18:07:51 -0300

    Initial commit, vqv!

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Search from './components/Search';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hello Word</h1>
+        <Search />
+      </>
+    );
+  }
+}
+
+export default App;

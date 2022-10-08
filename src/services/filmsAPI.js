ece26cb0f85515455128c5f7887a6fffa0fcb876
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-10-07 22:51:12 -0300

    Refatoração: remoção da requisição de busca por IDs e ajuste de código

--- src/services/filmsAPI.js
+++ src/services/filmsAPI.js
@@ -1,12 +1,5 @@
 export const getFilms = async () => {
   const request = await fetch("https://ghibliapi.herokuapp.com/films");
   const requestJson = await request.json();
-  return requestJson.results;
-};
-
-
-export const getFilmsById = async (id) => {
-  const request = await fetch(`https://ghibliapi.herokuapp.com/films/${id}`);
-  const requestJson = await request.json();
-  return requestJson.results;
+  return requestJson;
 };
--- src/services/locationsAPI.js
+++ src/services/locationsAPI.js
@@ -1,12 +1,5 @@
 export const getLocations = async () => {
   const request = await fetch("https://ghibliapi.herokuapp.com/locations");
   const requestJson = await request.json();
-  return requestJson.results;
-};
-
-
-export const getLocationById = async (id) => {
-  const request = await fetch(`https://ghibliapi.herokuapp.com/locations/${id}`);
-  const requestJson = await request.json();
-  return requestJson.results;
+  return requestJson;
 };

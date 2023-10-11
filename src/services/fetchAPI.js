c8c179f9cec4eff1b72cf16fb4f17538712217e0
Ana Luisa <85767799+analuisams99@users.noreply.github.com>
2023-10-11 01:12:28 -0300

    Modificação do caminho da API

--- src/services/fetchAPI.js
+++ src/services/fetchAPI.js
@@ -1,5 +1,5 @@
 export const fetchAPI = async (data) => {
-  const request = await fetch(`https://ghibliapi.herokuapp.com/${data}`);
+  const request = await fetch(`https://ghibliapi.vercel.app/${data}`);
   const requestJson = await request.json();
   return requestJson;
 };
